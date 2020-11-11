cd1127068f7d9cf12a334a784348fa082817e77c
JuliaGuimiot <julia.guimiot@huvrdata.com>
2020-11-11 16:17:15 -0600

    reformatted and sorted file imports

--- django_cloud_tasks/registries.py
+++ django_cloud_tasks/registries.py
@@ -7,12 +7,13 @@ class CloudTaskRegistry(object):
     """
     Registry of cloud tasks.
     """
+
     def __init__(self):
         self._registry = {}
 
     def register(self, base_task_class):
         self._registry[base_task_class.internal_task_name] = base_task_class
-        logger.info('Task {0} registered'.format(base_task_class.internal_task_name))
+        logger.info("Task {0} registered".format(base_task_class.internal_task_name))
 
     def get_task(self, name):
         return self._registry[name]
