59f263dfb0bdf9efeb0bd2a17e30b482d5ddeb4c
JuliaGuimiot <julia.guimiot@huvrdata.com>
2020-11-16 13:10:44 -0600

    fixing get_body

--- django_cloud_tasks/base.py
+++ django_cloud_tasks/base.py
@@ -314,21 +314,21 @@ class CloudTaskWrapper(object):
         task_name: string of task name
         """
 
-        
-        task_body = {
-            "http_request": {  # Specify the type of request.
-                "http_method": tasks_v2.HttpMethod.POST,
-                "url": self._task_handler_url,  # The full url path that the task will be sent to.
-                "oidc_token": {"service_account_email": DCTConfig.service_account_email()},
+        body = {
+            "task": {
+                "http_request": {  # Specify the type of request.
+                    "http_method": tasks_v2.HttpMethod.POST,
+                    "url": self._task_handler_url,  # The full url path that the task will be sent to.
+                    "oidc_token": {"service_account_email": DCTConfig.service_account_email()},
+                }
             }
-        }
 
         if payload is not None:
             if isinstance(payload, dict):
                 # Convert dict to JSON string
                 payload = json.dumps(payload)
                 # specify http content-type to application/json
-                task_body["http_request"]["headers"] = {
+                body["task"]["http_request"]["headers"] = {
                     "Content-type": "application/json"
                 }
 
@@ -336,7 +336,7 @@ class CloudTaskWrapper(object):
             task_payload = payload.encode()
 
             # Add the payload to the request.
-            task_body["http_request"]["body"] = task_payload
+            body["task"]["http_request"]["body"] = task_payload
 
         if in_seconds is not None:
             # Convert "seconds from now" into an rfc3339 datetime string.
@@ -347,7 +347,7 @@ class CloudTaskWrapper(object):
             timestamp.FromDatetime(d)
 
             # Add the timestamp to the tasks.
-            task_body["schedule_time"] = timestamp
+            body["task"]["schedule_time"] = timestamp
 
         if task_name is not None:
             # Add the name to tasks.
